8c187ccd737f66f9867e47fe6fb0a3897a565234
yanhock <davronbekyoqubovnew>
2023-09-05 23:03:08 +0500

    Bot Project

---
+++ cloud.py
@@ -0,0 +1,32 @@
+# Images Request
+
+coll_center_image = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-x_rPcvnk7eQEePgAL0wC0o5fx11OOVVmc18ZzYWA8N_e5vderIddz49AvajHWU2Ky0y5XYeiPi4CFT043kAuKFUfyNJg=w1376-h1003"
+discount_image = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-xA-uuv6lMwimcTb4_u1pjqjnNX_n75ZuVFsR_eXClqQaTNfixZ_g7giIEshP2l7UlMKfBCbreL0D-ZuhqXI-ki1Ddw-A=w1380-h968"
+
+# Ressults images
+
+image1 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-wPm_rS04HzUfbx9VjxsUfXiWw2iSmkeoKjmW8BjCf5lOmdKckmN-yfowbCwOfBx3UnWnUd30QZhu1buZvjnp9Adhuq=w1380-h968"
+image2 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-xie72ypbUh_Wisl-BwSpslmE15sfI9y9JgHyX8HnXfRA6pTXYCDQwyi2ydVqioKU_bwcc6txf3Bv8JlinQyrBa763xKw=w1380-h968"
+image3 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-zo6l4TIXIBY43ryiXwRXP2WaazOhvijMpk7sA3p5JbzjIiFwvryOZ-kaUvttlPT4LcsHc63dtFZU3Z8KQImvrcjZ8ikg=w1380-h968"
+image4 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-wiMMw9ax-BK1aW8hWj3HB1kN1zLasgYW93A5SU2hW-g7K0nA1qJCYHeSsXimb5dPnvk9xTJLP3f3fzJX3SaehGBKTw7A=w1380-h968"
+image5 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-x5geacxyUJvUBTnrFGgoaDlMJtugdZDhvJso3cnXYyENjbBdDKokmZK77GA1D-3oZbmaOyp7kiKk04wlrsJJ8uBSCaJg=w1380-h968"
+image6 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-y4hbMER6RfKRa40b-IHs09v1w_BiIA7H5gPXijcuslX_cufdUbMm1A43pO5X66oc2qJAcU_ZmAQGHwLTUQRbwL-xpKsg=w1380-h968"
+image7 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-x9AOWy4uhiK2DOb_ZqF7tWbvaBCqPvFVQz_8AJkewMarYzfL55GyDQ-q1yJUith_-FKuXSM2iCsmv6vsK5TnmQyBIEOA=w1380-h968"
+image8 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-z26F4CJebXL0z58u_FhvvRJs-OPf7DdWIX_jXum8B2QM8sYQLj0p_tj_oLMfA1AJQ8gNQDrRFIRgMPNMXKwT1KxVcO_w=w1380-h968"
+
+image9 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-xp1m_XzmDSwhGYtXHy5dprF2QhSZMKjuaFUvX5KttYTgZlZH92UZu00mRsfUgENkFuxykJ0XUdD9aHWEkZB_Cnpd_Shw=w1380-h967"
+image10 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-yKdsB6nVQM--0oxNyoo8C4kCVF1k6B8p_oLBnwLe8wHub4-PyLjtkj711IIHHiPp3FvptmNLq6Ky5UnQlIGJZ-95mM1g=w1380-h967"
+image11 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-w8We2C0AwBIVqvztKOXpNZEO19IE7oWzYNVSg7YvxURinIKMEZRehBY_pPZiVZaFG1-vByjsAV9uf3unELAPwA7c_mpg=w1380-h514"
+image12 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-x9uraJMvLxVAP6NqgwVQIlc5mrRKsaEQ1qVXrgugUDLksgWsiiK2W4unheyhqDuaHs4VtrVoOtvGXNPSQ3Tez8ZTusoA=w1380-h967"
+image13 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-z8eBnOnH6JAcKispTwxnmhz_6jEpmV6XFc8WWLU2O7v0HmupKsP4lLxEQpV3Yd7M5XG4iigPg_HmCsqTBP-hw2Tigxkg=w1380-h514"
+image14 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-y1cjr9ZBHCiACFNlFivb5q81vKnj5LwA3HDjASV42zjDZ6bU8YUbEt3aSmo2xI33laBwscSyzK2432-auyxsI9PlbR9g=w1380-h967"
+image15 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-wQTbGLMgvccRldt2BEzuOENp7vXtmc66JyLgr6yL-dT7lpmnuZQ7xUsaDUqPeQ_0nmy0GTFlfF9gyRLxlDduOePWZHaQ=w1380-h967"
+image16 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-wyGShnUDNFuuQLzDz9eN9nKDw5aW5zfKgHfFe4w-XA--Qlv5UpRQLEUBZ1gMiAYFxMS7HGInN1nUzd9GE0PkezUTY9=w1380-h967"
+image17 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-yk2jCo1nwYkG3Mtwnquwl5rQAQuJQX5RLKMUkbekrHg5HAmPMIlkjiMIToIlrP0-Z8Zms6oHpHwU9JPhqZl7QCdpFf=w1380-h514"
+image18 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-x-2cLrjKkh0pXvCK9465YXepLCv4yFeETezOwiURl_CXuAb7tu_g6k8aWlzY2yFldgORkubtXrbwtx-6J7-pGrPUuIgg=w1380-h967"
+image19 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-wmYdI1rji4aGxkyw1vSYcMv-MU69B2xvCyOJ6eI2cAjhDicCyWBeTjgZkThisx8ixjUVVKx6-pgfebNyMsXA23SJwrjg=w1380-h967"
+image20 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-woYm5AtVBejntwq4xuMUClEHO2VQqDGRWhFBUO_MhzEZ084ZlDzdqFwKhIRuGoGaRJAYwq5N3aB2rVAwGEYqzdGf-c=w1380-h967"
+image21 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-ymj8gnUAfwttVyimlhnZMUhuki51TWH2glpHwOELh-FoZN48PjAmVCPThzHxTmUFUs8ELDvKEhdGPdbedCRGO-GIFBEA=w1380-h967"
+image22 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-xqkOtnZvxkVMoer8wT8TNI7l6fivr-LzU2Uu-jLaUW8p50pYDwJ9z-vZjZQPgEkDmsM7qc3rkVtZLi9KbP9M28WSQEdw=w1380-h967"
+image23 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-z7NeusUtt-Hfgc-6AgNL3wD6kVIew8PhiTbs67At8THdd3RtavdzBbRevfNCIU6Nzh4O_Bd3vjSCzY1d9RNraAvoJb=w1380-h967"
+image24 = "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-wJpFq2vGJeNpRMz4klYaxCRSnM7QcHH3eoHuOKGH1N5Id3z6w4265CTO17l2xxe9lS5uBK8bBmenRwAncScRdLJZTH=w1380-h967"
